e0914dbf0f8a8f81c56eee8850ad5920399991f5
SayemKaderChowdhury <sayem.kader.chowdhury@g.bracu.ac.bd>
2020-06-26 00:11:22 +0600

    Created Task model

--- tasks/views.py
+++ tasks/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Ai on ogga app banaiam')
+    return render(request, 'tasks/list.html')
